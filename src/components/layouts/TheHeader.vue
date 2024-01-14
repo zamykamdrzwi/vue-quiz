af507b9b8cca2a0b709a773ae99dbd231b282ee3
Rafal <rafdaw1337@wp.pl>
2024-01-14 15:54:01 +0100

    dsa

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -14,14 +14,57 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto gap-3">
-          <li class="nav-item green">
-            Start Quiz
+          <li class="nav-item">
+            <a href="#" class="nav-link green">Start Quiz</a>
           </li>
-          <li class="nav-item green">
-            Add Question
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link green dropdow-toggle"
+            data-bs-toggle="dropdown">
+              Choose Quiz
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a href="#" class="dropdown-item green"
+                @click="chooseQuest(1)">Quiz 1</a></li>
+              <li><a href="#" class="dropdown-item green"
+                @click="chooseQuest(2)">Quiz 2</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link green">Add Question</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  provide() {
+    return{
+      choosedQuiz: this.quizNr,
+    };
+  },
+  data() {
+    return {
+      quizNr: '1',
+      dsadas: ''
+    };
+  },
+  methods: {
+    chooseQuest(nr) {
+      this.dsadas = nr;
+      console.log(this.choosedQuest)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-menu{
+  background-color: rgba(0,0,0,0.2) !important;
+}
+.dropdown-item:active{
+  background-color: rgba(0,0,0,0.2) !important;
+}
+</style>
--- src/components/quiz/TheQuiz.vue
+++ src/components/quiz/TheQuiz.vue
@@ -4,18 +4,21 @@
       <div class="card-header border-0">Question 1</div>
       <div class="card-body">
         <div class="card-title d-flex justify-content-center fs-2 pb-3">
-          {{ questions[0].quest }}
+          {{ questions[0][0].quest }}
         </div>
         <div class="row  gap-3">
           <answer-card
-            class="col-xl-3 m-auto">
-            {{ questions[0].answer1 }}</answer-card>
+            class="col-xl-3 m-auto"
+            @click="checkAnswer(1)">
+            {{ questions[0][0].answer1 }}</answer-card>
           <answer-card
-            class="col-xl-3 m-auto">
-            {{ questions[0].answer2 }}</answer-card>
+            class="col-xl-3 m-auto"
+            @click="checkAnswer(2)">
+            {{ questions[0][0].answer2 }}</answer-card>
           <answer-card
-            class="col-xl-3 m-auto">
-            {{ questions[0].answer3 }}</answer-card>        
+            class="col-xl-3 m-auto"
+            @click="checkAnswer(3)">
+            {{ questions[0][0].answer3 }}</answer-card>        
         </div>
       </div>
     </the-card>
@@ -25,30 +28,58 @@
 <script>
 import AnswerCard from '../UI/AnswerCard.vue';
 export default {
+  inject: ['choosedQuiz'],
   components: {
     AnswerCard
   },
   data() {
     return {
+      xd: 'das',
       questions: [
-        {
-          id: 1,
-          quest: 'Example questionpadding-left: 10px !important;padding-left: 10px !important;padding-left: 10px !important;',
-          answer1: 'Answer One  Answer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer One',
-          answer2: 'Answer Two padding-left: 10px !important; niger niger niger niger niger niger',
-          answer3: 'Answer Three nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger ',
-          correctAnswer: 2
-        },
-        {
-          id: 2,
-          quest: 'Second example question',
-          answer1: 'Answer One',
-          answer2: 'Answer Two',
-          answer3: 'Answer Three',
-          correctAnswer: 1
-        },
-      ]
+        [
+          {
+            id: 1,
+            quest: 'Example questionpadding-left: 10px !important;padding-left: 10px !important;padding-left: 10px !important;',
+            answer1: 'Answer One  Answer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer OneAnswer One',
+            answer2: 'Answer Two padding-left: 10px !important; niger niger niger niger niger niger',
+            answer3: 'Answer Three nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger ',
+            correctAnswer: 2
+          },
+          {
+            id: 2,
+            quest: 'Second example question',
+            answer1: 'Answer One',
+            answer2: 'Answer Two',
+            answer3: 'Answer Three',
+            correctAnswer: 1
+          }
+        ],
+        [
+          {
+            id: 1,
+            quest: 'Tab2 Second example question',
+            answer1: 'Answer One',
+            answer2: 'Answer Two',
+            answer3: 'Answer Three',
+            correctAnswer: 1
+          },
+          {
+            id: 2,
+            quest: 'Tab2 dsadsadasSecond example question',
+            answer1: 'Answer One',
+            answer2: 'Answer Two',
+            answer3: 'Answer Three',
+            correctAnswer: 1
+          }
+        ]
+      ],
     };
+  },
+  methods: {
+    checkAnswer(nr) {
+      console.log(nr);
+      console.log(`dsada ${this.choosedQuiz}`)
+    }
   },
 }
 </script>
