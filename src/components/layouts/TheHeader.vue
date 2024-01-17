38cfdd4c5670a2c0a45546b24cdeb4cf4c909952
Rafal <rafdaw1337@wp.pl>
2024-01-17 11:44:53 +0100

    fixed header - now you can close menu by click anywher

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -9,17 +9,17 @@
         </a>
       </div>
       <button class="navbar-toggler shadow-none border-0 fs-3" type="button"
-        data-bs-toggle="collapse"
+        data-bs-toggle="collapse" @click="showMenu"
         data-bs-target="#navbarNav">
         <span class="green">
           <font-awesome-icon :icon="['fas', 'ellipsis-vertical']" />
         </span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse" id="navbarNav" :class="{ 'hide-menu': menuVisible }">
         <ul class="navbar-nav ms-auto gap-3">
           <li class="nav-item dropdown">
             <a href="#" class="nav-link green dropdow-toggle"
-            data-bs-toggle="dropdown" @click="updateQuizList">
+            data-bs-toggle="dropdown" @click="updateQuizList" ref="dropdownMenu">
               Choose Quiz
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
@@ -46,6 +46,7 @@ import quizNames from '../quiz/quizNames.json';
 export default {
   data() {
     return {
+      menuVisible: false,
       quizList: ['Quiz 1', 'Quiz 2', 'Mateuszowy Quiz']
     };
   },
@@ -58,8 +59,20 @@ export default {
     },
     updateQuizList() {
       this.quizList = quizNames;
+    },
+    showMenu() {
+      console.log('dziala')
+      if(this.$refs.dropdownMenu.contains(event.target)){
+        return;
+      }
+      else{
+        this.menuVisible = !this.menuVisible;
+      }
     }
   },
+  mounted() {
+    document.addEventListener('click', this.showMenu);
+  }
 }
 </script>
 
@@ -69,5 +82,8 @@ export default {
 }
 .dropdown-item:active{
   background-color: rgba(0,0,0,0.2) !important;
+}
+.hide-menu {
+  position: none;
 }
 </style>
